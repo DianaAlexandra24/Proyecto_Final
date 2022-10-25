d662b92989d929d9e04a4479e0e5fd35ff24f0f9
DianaAlexandra24 <74255388+DianaAlexandra24@users.noreply.github.com>
2022-10-25 20:57:35 +0200

    HalfResponsive

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,16 +5,16 @@
         <section class="  w-screen  ">
             <div class=" w-full h-full flex relative">
             <div class=" bg-gradient-to-t from-black w-full h-1/2   ">
-                <img  src="public\galeria\80594.jpg"  class=" w-full "> <img>
-                <router-link to="/Morefilms"> <button class=" rounded-lg bg-white py-2 justify-center item-center flex h-12 w-48  ml-12 absolute bottom-10">
+                <img  src="public\galeria\80594.jpg"  class="object-cover md:h-full md:w-full "> <img>
+                <router-link to="/Morefilms"> <button class=" rounded-lg bg-white py-2 justify-center item-center flex h-12 w-48  ml-12 absolute bottom-10 invisible  sm:visible">
                     <span class="text-gray-800 font-semibold "> <i class="fa-solid fa-play "> </i>  Reproducir</span>
                 </button></router-link>
 
               
-                <button  class="rounded-lg bg-transparent py-2 justify-center flex h-12 w-48  ml-80 absolute  item-center bottom-10">
+                <router-link to="/Morefilms"> <button  class="rounded-lg bg-transparent py-2 justify-center flex h-12 w-48  ml-80 absolute  item-center bottom-10  invisible sm:visible" muted loop>
                  <span class="text-gray-100 font-semibold"><i class="fa-solid fa-circle-info"></i> Mas Informacion</span>
-                 
-                 </button>       
+
+                 </button>  </router-link>
 
             </div>
           
--- src/components/PeliculasAPI.vue
+++ src/components/PeliculasAPI.vue
@@ -3,18 +3,22 @@
     <div class=" mt-20">
           <div class="flex   ml-10"> <h2 class="text-white text-2xl font-semibold	mb-0 "> Tendencias ahora</h2></div>
       <swiper 
-        :slidesPerView="6"
+        :slidesPerView=" 6 " 
         :spaceBetween="10"
         :slidesPerGroup="3"
         :loop="true"
         :loopFillGroupWithBlank="false"
+  
         :pagination="{
           clickable: true,
         }"
+        
+   
         :navigation="true"
         :modules="modules"
-        class="mySwiper mt-10  "> 
-        <swiper-slide v-for="film in data" :key="film.id"><router-link :to="`Films/${film.id}`"><img class="hover:scale-110 transition duration-300 ease-in-out" :src="`https://image.tmdb.org/t/p/w500/${film.poster_path}`"></router-link></swiper-slide>
+       
+        class="mySwiper mt-10 overflow-hidden "> 
+        <swiper-slide v-for="film in data" :key="film.id"><router-link :to="`Films/${film.id}`"><img class=" hover:scale-110 transition duration-300 ease-in-out h-48  w-full  object-cover " :src="`https://image.tmdb.org/t/p/w500/${film.poster_path}`"></router-link></swiper-slide>
         </swiper>
     </div>
     <div class="mt-20 ">
@@ -30,9 +34,11 @@
       }"
       :navigation="true"
       :modules="modules"
-      class="mySwiper mt-10  "> 
-      <swiper-slide v-for="film in data" :key="film.id"><router-link :to="`Films/${film.id}`"><img class="hover:scale-110 transition duration-300 ease-in-out" :src="`https://image.tmdb.org/t/p/w500/${film.poster_path}`"></router-link></swiper-slide>
+      class="mySwiper mt-10 "> 
+      <swiper-slide v-for="film in data" :key="film.id"><router-link :to="`Films/${film.id}`"><img class="hover:scale-110 transition duration-300 ease-in-out h-48  w-full flex flex-row sm:flex-col object-cover" :src="`https://image.tmdb.org/t/p/w500/${film.poster_path}`"></router-link></swiper-slide>
       </swiper>
+
+      
       
     </div>
 </div>
@@ -61,10 +67,19 @@ export default {
           loading:false,
         }
       },
+      
     components: {
     Swiper,
     SwiperSlide,
   },
+  breakpoints: {
+          300: {
+            slidesPerView: 2
+          },
+          700: {
+      slidesPerView: 6
+    }
+        },
     methods:{
           getFilms(){
             axios.get('https://api.themoviedb.org/3/movie/popular?api_key=b22a57acb6df2a99f477aab48d4a36c3&language=en-US&page=1')
